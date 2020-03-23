99457eda1446b410806179ba9f62a6f06500eeec
Jason <jason1996429@gmail.com>
2020-03-22 23:42:42 -0700

    more options and updated schema

--- models/orders.js
+++ models/orders.js
@@ -11,6 +11,10 @@ const addrSchema = new Schema({
 });
 const ordersSchema = new Schema({
     "model_url": String,
+    "material": {type: String, enum:["resin", "pbt", "abs"]},
+    "color": {type: String, enum:["black", "white"]},
+    "connectivity": {type: String, enum:["2.4ghz", "bt", "both"]},
+    "sensor": {type: String, enum:["avago", "pixart"]},
     "price": Number,
     "shipping_addr": [addrSchema],
     "status": {type: String, enum:["Paid", "Shipped", "Delivered", "Canceled", "Refunded", "Pending"]},
