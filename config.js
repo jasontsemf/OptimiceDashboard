a886842a68b789dab25dd6217ede31a7e715465b
Jason <jasontsemf@gmail.com>
2023-07-25 00:16:47 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,10 @@
 require('dotenv').config();
+// module.exports = {
+//   PORT: process.env.PORT || 3000,
+//   MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/test'
+// }
+
 module.exports = {
-  PORT: process.env.PORT || 3000,
+  PORT: 8080,
   MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/test'
 }
